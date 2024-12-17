25d3f6be54c86126eb5cc71419d87cfa9fc25800
gr8monk3ys <lorenzosca7@gmail.com>
2024-12-17 12:02:25 -0800

    feat(layout): add footer to all pages

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { siteConfig } from "@/config/site"
 import { fontSans } from "@/lib/fonts"
 import { cn } from "@/lib/utils"
 import { SiteHeader } from "@/components/site-header"
+import { SiteFooter } from "@/components/site-footer"
 import { TailwindIndicator } from "@/components/tailwind-indicator"
 import { ThemeProvider } from "@/components/theme-provider"
 
@@ -43,6 +44,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
             <div className="relative flex min-h-screen flex-col">
               <SiteHeader />
               <div className="flex-1">{children}</div>
+              <SiteFooter />
             </div>
             <TailwindIndicator />
           </ThemeProvider>
